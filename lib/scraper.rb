22d2af721ad06ecb170f8115f46b9169d0545615
Peyton <pekellogg@hotmail.com>
2021-10-14 15:36:32 -0700

    commit tree 7fa2cbf090ad6d26f280da3d5f528f19a20d253c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
+require "awesome_print"
 
 html = URI.open("https://flatironschool.com/")
+doc = Nokogiri::HTML5(html)
+doc.css(".headline-26OIBN").text
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+#courses.each{|course|puts course.text.strip}
+
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
